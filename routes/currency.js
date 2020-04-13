49a3e5c68f71dd3503dfdff4c692c27de37960e4
RomanSnig <51839680+RomanSnig@users.noreply.github.com>
2020-04-13 17:33:49 +0300

    Update currency.js

--- routes/currency.js
+++ routes/currency.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const {findByQuantityNum, getAll} = require('../controllers/exchangeRate');
 
-router.get('/currency/:currency', findByQuantityNum);
+router.get('/currency/:quantity', findByQuantityNum);
 router.get('/currencies', getAll);
 
 module.exports = router;
